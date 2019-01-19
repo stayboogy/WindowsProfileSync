017852682525fc43c795ccf397743fbed5a7a229
unknown <idaboogerman@gmail.com>
2019-01-19 11:24:57 -0600

    if error = Continue

--- ProfileSync.cmd
+++ ProfileSync.cmd
@@ -1,9 +1,9 @@
 timeout /t 1
-XCOPY "C:\Users\Administrator\Desktop\*.*" "D:\CCLONE\Desktop\" /S /K /D /H /Y /E
-XCOPY "C:\Users\Administrator\Documents\*.*" "D:\CCLONE\Documents\" /S /K /D /H /Y /E
-XCOPY "C:\Users\Administrator\Pictures\*.*" "D:\CCLONE\Pictures\" /S /K /D /H /Y /E
-XCOPY "C:\Users\Administrator\Downloads\*.*" "D:\CCLONE\Downloads\" /S /K /D /H /Y /E
-XCOPY "C:\Users\Administrator\Music\*.*" "D:\CCLONE\Music\" /S /K /D /H /Y /E
-XCOPY "C:\Users\Administrator\Videos\*.*" "D:\CCLONE\Videos\" /S /K /D /H /Y /E
+XCOPY "C:\Users\Administrator\Desktop\*.*" "D:\CCLONE\Desktop\" /C /S /K /D /H /Y /E
+XCOPY "C:\Users\Administrator\Documents\*.*" "D:\CCLONE\Documents\" /C /S /K /D /H /Y /E
+XCOPY "C:\Users\Administrator\Pictures\*.*" "D:\CCLONE\Pictures\" /C /S /K /D /H /Y /E
+XCOPY "C:\Users\Administrator\Downloads\*.*" "D:\CCLONE\Downloads\" /C /S /K /D /H /Y /E
+XCOPY "C:\Users\Administrator\Music\*.*" "D:\CCLONE\Music\" /C /S /K /D /H /Y /E
+XCOPY "C:\Users\Administrator\Videos\*.*" "D:\CCLONE\Videos\" /C /S /K /D /H /Y /E
 timeout /t 30
 exit
