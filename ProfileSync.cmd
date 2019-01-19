5c186b0aeb7be6516102803aae9107d7d827b364
unknown <idaboogerman@gmail.com>
2019-01-19 11:18:38 -0600

    so i can actually review

--- ProfileSync.cmd
+++ ProfileSync.cmd
@@ -5,5 +5,5 @@ XCOPY "C:\Users\Administrator\Pictures\*.*" "D:\CCLONE\Pictures\" /S /K /D /H /Y
 XCOPY "C:\Users\Administrator\Downloads\*.*" "D:\CCLONE\Downloads\" /S /K /D /H /Y /E
 XCOPY "C:\Users\Administrator\Music\*.*" "D:\CCLONE\Music\" /S /K /D /H /Y /E
 XCOPY "C:\Users\Administrator\Videos\*.*" "D:\CCLONE\Videos\" /S /K /D /H /Y /E
-timeout /t 3
+timeout /t 30
 exit
