72b553a7061261e2d8213ea2d1023216189aa8ed
unknown <idaboogerman@gmail.com>
2019-01-19 11:16:43 -0600

    initial commit

---
+++ ProfileSync.cmd
@@ -0,0 +1,9 @@
+timeout /t 1
+XCOPY "C:\Users\Administrator\Desktop\*.*" "D:\CCLONE\Desktop\" /S /K /D /H /Y /E
+XCOPY "C:\Users\Administrator\Documents\*.*" "D:\CCLONE\Documents\" /S /K /D /H /Y /E
+XCOPY "C:\Users\Administrator\Pictures\*.*" "D:\CCLONE\Pictures\" /S /K /D /H /Y /E
+XCOPY "C:\Users\Administrator\Downloads\*.*" "D:\CCLONE\Downloads\" /S /K /D /H /Y /E
+XCOPY "C:\Users\Administrator\Music\*.*" "D:\CCLONE\Music\" /S /K /D /H /Y /E
+XCOPY "C:\Users\Administrator\Videos\*.*" "D:\CCLONE\Videos\" /S /K /D /H /Y /E
+timeout /t 3
+exit
